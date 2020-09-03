582facf23808a6183e205597bff6e42621beffbe
Thomas Hedegaard <thomashj93@gmail.com>
2020-09-03 07:40:19 +0200

    add migration scripts for BookModel

---
+++ Demo_BookApp/BookListRazor/Model/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookListRazor.Model
+{
+    public class ApplicationDbContext : DbContext 
+    {
+
+
+        // we have to pass the DbContext options, to the parent class for DI
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        // in order to add anything to the DB, we need an entry
+        public DbSet<BookModel> Book { get; set; }
+
+
+    }
+}
---
+++ Demo_BookApp/BookListRazor/Pages/BookList/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookListRazor.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookListRazor
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+        public IEnumerable<BookModel> Books { get; set; }
+
+        public IndexModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        // TODO: can I call this OnGetAsync?
+        public async Task OnGet()
+        {
+            // assign books to be all of the books from the database
+            Books = await _db.Book.ToListAsync();
+        }
+    }
+}
