464fe39bce00972300ad36ef6397acdeda08b357
Thomas Hedegaard <thomashj93@gmail.com>
2020-09-07 08:27:37 +0200

    add logic to BookList/Create pagemodel

--- Demo_BookApp/BookListRazor/Pages/BookList/Create.cshtml.cs
+++ Demo_BookApp/BookListRazor/Pages/BookList/Create.cshtml.cs
@@ -12,6 +12,10 @@ namespace BookListRazor
     {
         private readonly ApplicationDbContext _db;
 
+        // bind to the view's Book props entered in the form
+        // this way, we won't need to pass in the bookObj, 
+        // when invoking the Post Method later
+        [BindProperty]
         public BookModel Book { get; set; }
 
         public CreateModel(ApplicationDbContext db)
@@ -22,6 +26,29 @@ namespace BookListRazor
         public void OnGet()
         {
 
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            // check if the incoming request bind to the model props correctly
+            // if valid, add to database
+            if (ModelState.IsValid)
+            {
+                // not added to DB yet - but added a que
+                await _db.Book.AddAsync(Book);
+                // push data in EF's que, to the DB
+                await _db.SaveChangesAsync();
+                return RedirectToPage("Index");
+            }
+            else
+            {
+                return Page();
+            }
+
+
+
+
+            throw new NotImplementedException();
         }
     }
 }
