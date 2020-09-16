550fd148bbf45d534c7d850a1612272c778374c9
Thomas Hedegaard <thomashj93@gmail.com>
2020-09-16 08:14:43 +0200

    fix BookId binding issue

--- Demo_BookApp/BookListRazor/Pages/BookList/Edit.cshtml.cs
+++ Demo_BookApp/BookListRazor/Pages/BookList/Edit.cshtml.cs
@@ -11,6 +11,7 @@ namespace BookListRazor
     public class EditModel : PageModel
     {
         private ApplicationDbContext _dbContext;
+        [BindProperty]
         public BookModel Book { get; set; }
 
         public EditModel(ApplicationDbContext dbContext)
@@ -22,5 +23,25 @@ namespace BookListRazor
         {
             Book = await _dbContext.Book.FindAsync(id);
         }
+
+        //we'll be redirecting to a page, so we return IActionResult
+        public async Task<IActionResult> OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                // retrieve book we wanna change from DB
+                var bookFromDb = await _dbContext.Book.FindAsync(Book.Id);
+                bookFromDb.Name = Book.Name;
+                bookFromDb.ISBN = Book.ISBN;
+                bookFromDb.Author = Book.Author;
+
+                // save model changes to db
+                await _dbContext.SaveChangesAsync();
+
+                return RedirectToPage("Index");
+            }
+            return RedirectToPage();
+        }
+
     }
 }
